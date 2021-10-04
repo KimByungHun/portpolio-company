df291a1e72fd646833047a235095992ca498e32c
KimByungHun <k01087831451@gmail.com>
2021-10-04 23:56:39 +0900

    [update] checkBox, toggle메뉴 수정_1

---
+++ js/toggle.js
@@ -0,0 +1,25 @@
+
+//DOM Caching
+
+
+
+
+
+// TermBtnCall.onclick = function(e){
+//     e.preventDefault(); 
+
+//     var isTermOn = $(this).hasClass("on");
+
+//     TermBtnCall.classList.toggle("on"); 
+
+//     if(isTermOn){
+//         $(this).removeClass("on");
+//     }else{
+
+//         $(this).addClass("on");
+
+//     }
+
+    
+
+// }
