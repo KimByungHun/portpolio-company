67e9d0de914ccf1e613a0334d9ce78795cd4c845
KimByungHun <k01087831451@gmail.com>
2021-10-26 17:37:09 +0900

    gallery loading (float left수정필요)

--- js/gallery.js
+++ js/gallery.js
@@ -1,76 +1,94 @@
 $.ajax({
     url:"https://www.flickr.com/services/rest/?method=flickr.photos.search",
+    //url:"https://www.flickr.com/services/rest/?method=flickr.interestingness.getList", 
     dataType:"json", 
     data:{
         api_key:"e4dac3c336f1aa366b20a39011946539", 
-        // datetime: "4",
-        per_page:21, 
+        per_page:100, 
         format:"json",
-        nojsoncallback:1,
+        nojsoncallback:1, 
         privacy_filter : 1, 
         tags :"mordern art"
     }
 })
-.success(function(data){
-    console.log(data.photos.photo); 
-    let items = data.photos.photo; 
-
+.success(function(data){    
     
-    $("#gallery").append("<ul class='ul'>");
-
+    let items = data.photos.photo; 
+    console.log(items);
+   
+    $("#gallery").append("<ul>");
+   
     $(items).each(function(index, data){
-
-        let text = data.title; 
-        if(!data.title){
+        let text = data.title;        
+        if(!data.title){          
             text = "No description in this photo"; 
         }
 
-        $("#gallery .ul")
-            .append(
-                $("<li class='li'>")
+        $("#gallery ul")
+        .append(
+            $("<li>")
+                .append(
+                    $("<a>").attr({
+                        href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                    })
                     .append(
-                        $("<a>").attr({
-                            href : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
+                        $("<img>").attr({
+                            src : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_m.jpg"
                         })
+                    )
+                )
+                .append(
+                    $("<h2>").text(text)
+                )
+                .append(
+                    $("<div class='profile'>")
                         .append(
-                            $("<img>").attr({
-                                src : "https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_m.jpg"
-                            })
-                        )
-                    )
-                    .append(
-                        // $("<span>").text("TITLE"),
-                        $("<h2>").text(text)
-                    )
-                    .append(
-                        $("<div class='profile'>")
-                            .append(
-                                $("<div class='left'>").append(
-                                    // $("<span>").text("owner"),
-                                    $("<span>").text("date"),
-                                    // $("<span>").text("tags")
+                            $("<div class='left'>").append(
+                                $("<span>").text("date")
+                            ),
+                            $("<div class='right'>").append(
 
-                                ),
-                                
-                                // $("<img>").attr({
-                                //     src : "https://www.flickr.com/buddyicons/"+data.owner+".jpg"
-                                // }),
-                                $("<div class='right'>").append(
-                                    // $("<img>").attr({
-                                    //     src : "https://www.flickr.com/buddyicons/"+data.owner+".jpg"
-                                    // })
-                                    $("<span>").text(data.owner)
-                                    // $("<span>").text(data.time)
-
-                                )
+                                $("<span>").text(data.owner)
                             )
-                    )
-            )
-    })
+                        )
+                )
+        )
+    });  
+    
+    const total = $("#gallery ul li").length;
+
+    
+    let imgNum=0;
+
+    $("#gallery img").each(function(index, data){   
+        data.onerror = function(){
+            $(data).attr("src", "img/default.jpg");
+        }
+        
+        data.onload = function(){            
+            imgNum++;
+            console.log(imgNum);
+           
+            if(imgNum === total){   
+                $(".loading").addClass("off");
+
+                new Isotope("#gallery ul",{
+                    itemSelector : "#gallery ul li",
+                    columnWidth: "#galley ul li",                  
+                    transitionDuration: "0.5s"
+                });   
+                
+                $("#gallery ul").addClass("on");
+            }
+        }        
+    }); 
+    
+    
+    
 })
 .error(function(err){
     console.err("데이터를 호출하는데 실패했습니다"); 
-})
+});
 
 
 $("body").on("click", "#gallery ul li", function(e){
@@ -90,11 +108,3 @@ $("body").on("click", "#gallery ul li", function(e){
 $("body").on("click", ".pop span", function(){
     $(".pop").remove(); 
 });
-
-// window.addEventListener("load", ()=>{
-//     const grid = new Isotope(".ul",{
-//         itemSelector : ".li",
-//         columWidth: ".li",
-//         transitionDuration : "0.5s"
-//     });
-// });
