adfb7617efc44f2f33fbdc9ef47bd68f4d7097f1
KimByungHun <k01087831451@gmail.com>
2021-11-13 23:15:50 +0900

    #brand grid 형태

---
+++ js/isotope-sizer.js
@@ -0,0 +1,8 @@
+new Isotope("#brand .grid", {
+    itemSelector : ".item",
+    percentPosition : true,
+    transitionDuration : "0.8s",
+    masonry: { 
+        columnWidth : ".item-sizer"
+    }
+})
