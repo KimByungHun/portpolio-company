a283a7fb02c3f3f9e593d068dbc3f4b1b0ad2ad8
KimByungHun <k01087831451@gmail.com>
2021-11-29 14:35:41 +0900

    visualSlider auto rolling

--- js/visual_slide.js
+++ js/visual_slide.js
@@ -3,20 +3,36 @@ const $slider2 = $("#slider2");
 const $prev = $(".prev"); 
 const $next = $(".next"); 
 let speed = 1000; 
+let visual_timer;
+
+let enableClick = true; 
+
 
 init($slider1); 
 init($slider2);  
 
 $next.on("click", function(e){
 e.preventDefault();    
-next($slider1); 
-next($slider2); 
+if(enableClick){
+
+    enableClick=false;
+    next($slider1); 
+    next($slider2); 
+}
+
 }); 
 
 $prev.on("click", function(e){
 e.preventDefault();     
-prev($slider1); 
-prev($slider2); 
+
+    if(enableClick){
+
+        enableClick=false;
+        prev($slider1); 
+        prev($slider2); 
+    }
+
+
 }); 
 
 function init(el){
@@ -30,6 +46,7 @@ function next(el){
 el.children("ul").animate({ marginLeft : "-200%"}, speed, function(){
     $(this).css({ marginLeft : "-100%"}); 
     $(this).children("li").first().appendTo(this); 
+    enableClick = true;
 });
 }
 
@@ -38,5 +55,26 @@ function prev(el){
 el.children("ul").animate({marginLeft : "0%"},speed, function(){
     $(this).css({ marginLeft : "-100%"}); 
     $(this).children("li").last().prependTo(this); 
+    enableClick = true;
 }); 
 }
+
+$(".start").on("click", function(e){
+    e.preventDefault(); 
+
+    visual_timer = setInterval(function(){
+        next($slider1); 
+        next($slider2); 
+    },4000);
+    
+    $(".start").addClass("on"); 
+    $(".stop").removeClass("on"); 
+}); 
+
+$(".stop").on("click", function(e){
+    e.preventDefault(); 
+    clearInterval(visual_timer); 
+
+    $(".start").removeClass("on"); 
+    $(".stop").addClass("on");
+});
